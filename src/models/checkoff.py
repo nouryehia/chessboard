b868edbf834365f1378364334a3bdc7bb8686499
Sravya Balasa <sravya.balasa@gmail.com>
2019-11-01 15:22:19 -0700

    Wrote checkoff model

---
+++ src/models/checkoff.py
@@ -0,0 +1,22 @@
+from app import db
+
+
+class Checkoff(db.Model):
+    """
+    Represents a checkoff in the DB with relevant fields
+    of what user can be checked off for.\n
+    Fields:
+    id --> Checkoff ID. Unique, primary key\n
+    description --> Description of checkoff\n
+    name --> Corresponding assignment\n
+    suite_id --> ID of assignment checkoff is for\n
+    points --> Number of points checkoff is worth\n
+    @author sravyabalasa
+    """
+    __tablename__ = 'Checkoff'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    suite_id = db.Column(db.Integer, nullable=False)
+    points = db.Column(db.Integer, db.ForeignKey(CheckoffSuite.id),
+                       nullable=False, default=1)
