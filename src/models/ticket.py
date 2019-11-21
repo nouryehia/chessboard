f7a8292825143a024161a29ca0512bfc4cbfec3c
Yixuan Zhou <thomaszhou2333@gmail.com>
2019-11-21 15:13:33 -0800

    Working ticket

--- src/models/ticket.py
+++ src/models/ticket.py
@@ -2,7 +2,11 @@ from app import db
 from enum import Enum
 from typing import List
 from ticket_tag import Types
-# from datetime import datetime
+from datetime import datetime
+
+
+# Only 3 tags allowed per ticket
+MAX_TAG_NUM = 3
 
 
 class Status(Enum):
@@ -47,6 +51,10 @@ class Ticket(db.Model):
     is_private --> Whether this ticket is private.\n
     accepted_at --> The time that this ticket was accepted.\n
     help_type --> The type of help student need.\n
+    tag_one --> The ticket tag for this ticket. Not nullable since at least\
+                a tag.\n
+    tag_two --> The ticket tag for this ticket. Nullable.\n
+    tag_three --> The ticket tag for this ticket. Nullable.\n
     """
     __tablename__ = 'Ticket'
     id = db.Column(db.Integer(20), primary_key=True, nullable=False)
@@ -65,10 +73,16 @@ class Ticket(db.Model):
     is_private = db.Column(db.Boolean, nullable=False)
     accepted_at = db.Column(db.DateTime, nullabble=True)
     help_type = db.Column(db.Integer(11), nullable=False)
+    tag_one = db.Column(db.Integer(20), db.ForeignKey('ticket_tag.id'),
+                        nullable=False)
+    tag_two = db.Column(db.Integer(20), db.ForeignKey('ticket_tag.id'),
+                        nullable=True)
+    tag_three = db.Column(db.Integer(20), db.ForeignKey('ticket_tag.id'),
+                          nullable=True)
 
     def update(self, title: str, description: str, room: str,
                workstation: str, isPrivate: bool, help_type: HelpType,
-               tagStrings: List[Types]) -> None:
+               tag_list: List[Types]) -> None:
         """
         This method updates the current ticket. By taking in the updates from
         front end, this will update the corresponding tickit in the database.
@@ -86,3 +100,35 @@ class Ticket(db.Model):
         self.workstation = workstation
         self.isPrivate = isPrivate
         self.help_type = help_type
+
+        # Update the tags
+        self_tag_list = []
+        for i in range(0, MAX_TAG_NUM):
+            if (i < len(tag_list)):
+                self_tag_list.append(tag_list[i])
+            else:
+                self_tag_list.append(None)
+
+        # Assign the instance tags from the list
+        self.tag_one = self_tag_list[0]
+        self.tag_two = self_tag_list[1]
+        self.tag_three = self_tag_list[2]
+
+        # Do I need to commit to the database here or no?
+
+    def get_help_time_in_second(self) -> float:
+        """
+        Get the time in second of the time that this ticket spent\
+        to be resolved.\n
+        Return:\n
+        The difference in time.\n
+        """
+        return (self.closed_at - self.created_at).total_second()
+
+    def second_to_minute(self, second: float) -> float:
+        """
+        Convert to minute from second.\n
+        Return:\n
+        The second in float format.\n
+        """
+        return second / 60
