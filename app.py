4457b825e7391b3032d056a0213a2d66c03d9623
Simonne Contreras <42155386+sccontre@users.noreply.github.com>
2020-01-17 17:28:50 -0800

    Section DB (#60)
    * Initial work done for user.py file
    
    * Figuring out whats going on lol
    
    * A little bit of progress.
    
    * Somewhat working, need to figure out sorting a list and make sure some syntax is correct
    
    * Fixed some query parameters
    
    * Final changes to section - there are function definitions in comment above that belong in other files.
    
    * Typo fix
    
    Co-authored-by: Nate Browne <npcompletenate@protonmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask_api import FlaskAPI
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = FlaskAPI(__name__)
 CORS(app)
+db = SQLAlchemy(app)
+db.init_app(app)
 
 
 @app.route('/')
