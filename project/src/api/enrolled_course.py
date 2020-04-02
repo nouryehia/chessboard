32aa4c0f4656be2841b1cf376ced608c11184bd4
Nate Browne <npcompletenate@protonmail.com>
2020-04-02 16:36:36 -0700

    Adding in flask login and CORS support

--- project/src/api/enrolled_course.py
+++ project/src/api/enrolled_course.py
@@ -1,13 +1,17 @@
+from flask_cors import CORS
 from flask import Blueprint, request, jsonify
+from flask_login import login_required
 
 from ..models.enrolled_course import Role
 from ..models import enrolled_course as ec
 
 
 enrolled_course_api_bp = Blueprint('enrolled_course_api', __name__)
+CORS(enrolled_course_api_bp, supports_credentials=True)
 
 
 @enrolled_course_api_bp.route('/enroll_user', methods=['POST'])
+@login_required
 def enroll_user():
     """
     Route to enroll a user in to a specific section of a course.
@@ -29,6 +33,7 @@ def enroll_user():
 
 
 @enrolled_course_api_bp.route('/delete_user_from_course', methods=['POST'])
+@login_required
 def delete_user_from_course():
     """
     The route to remote an user from a particular course.
@@ -43,6 +48,7 @@ def delete_user_from_course():
 
 
 @enrolled_course_api_bp.route('/get_user_in_course', methods=['GET'])
+@login_required
 def get_user_of_course():
     """
     Route to get a user from a specific course.
@@ -54,6 +60,7 @@ def get_user_of_course():
 
 
 @enrolled_course_api_bp.route('/get_all_user_in_course', methods=['GET'])
+@login_required
 def get_all_user():
     """
     Route to get all the users of that is in a given course.
@@ -64,6 +71,7 @@ def get_all_user():
 
 @enrolled_course_api_bp.route('/get_user_in_all_course',
                               methods=['GET'])
+@login_required
 def get_user_in_all_course():
     """
     Route to get all the courses that a user is in.
@@ -76,6 +84,7 @@ def get_user_in_all_course():
 
 
 @enrolled_course_api_bp.route('/find_active_tutor_for', methods=['GET'])
+@login_required
 def find_active_tutor_for():
     queue_id = request.json['queue_id']
     return jsonify(ec.find_active_tutor_for(queue_id=queue_id))
