57efe94217004c382c5a15ea2c8e660605093ad7
Adory Vo <adoryvo.business@gmail.com>
2021-11-08 12:18:45 -0800

    Update user tests

--- project/tests/test_user.py
+++ project/tests/test_user.py
@@ -10,7 +10,10 @@ emails = [
     'prof@gmail.com'
 ]
 password = '$pbkdf2-sha256$29000$tLYWAgBAiLGWsvbeuxdijA$mbwptJE6FEUx2MoZM489.F/aYZ9Kn/99hC5DM.jSWG4'
-
+student_user = User.find_by_pid_email_fallback('', 'student@gmail.com')
+tutor_user = User.find_by_pid_email_fallback('', 'tutor@gmail.com')
+lead_user = User.find_by_pid_email_fallback('', 'lead@gmail.com')
+prof_user = User.find_by_pid_email_fallback('', 'prof@gmail.com')
 
 # Tests
 ## Methods used in the controller
@@ -30,6 +33,13 @@ def test_check_password():
 def test_find_by_email_fallback():
     # Test empty pid check
     assert User.find_by_pid_email_fallback('', '') is None
+
+    # Test find by pid
+    student_user.pid = '1'
+    student_user.save()
+    assert User.find_by_pid_email_fallback('1', '') is not None
+
+    # Test find by email
     for email in emails:
         assert User.find_by_pid_email_fallback('', email) is not None
 
@@ -39,11 +49,49 @@ def test_find_by_email_fallback():
         assert User.find_by_pid_email_fallback('some invalid pid', email) is not None
 
 
-def test_reset_password():
+def test_update_login_timestamp():
     user = User()
-    user.email = 'student@gmail.com'
 
-    user.reset_password('password')
+    old_login = user.last_login
+    user.update_login_timestamp()
 
-    # Requires database connection
-    assert User.check_password(user.email, 'password')
+    assert old_login != user.last_login
+
+
+def test_generate_token():
+    user = User()
+
+    old_token = user.token
+    user.generate_token()
+
+    assert old_token != user.token
+
+
+def test_request_promotion():
+    # Test no params
+    assert User.request_promotion() == (False, 'missing param', None)
+
+    # Test invalid user id & email
+    assert User.request_promotion(-1) == (False, 'user not found', None)
+    assert User.request_promotion(None, 'notemail') == (False, 'user not found', None)
+
+    # Test instructor user
+    assert User.request_promotion(None, 'lead@gmail.com') == (False, 'already an instructor', lead_user)
+    assert User.request_promotion(None, 'prof@gmail.com') == (False, 'already an instructor', prof_user)
+
+    # Test successful request
+    assert User.request_promotion(None, 'tutor@gmail.com') == (True, 'success', tutor_user)
+    assert tutor_user.request == True
+
+    # Test already requesting check
+    assert User.request_promotion(None, 'tutor@gmail.com') == (False, 'already requested', tutor_user)
+
+
+def test_reset_password():
+    student_user.reset_password('new_password')
+
+    assert User.check_password('student@gmail.com', 'new_password')
+
+    # Revert to old password
+    student_user.reset_password('password')
+
